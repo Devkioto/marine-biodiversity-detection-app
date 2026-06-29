9db48c2359c5d23f19e35c3c364a7737a9ab360f
abd ellah ikkou <mouniraz772@gmail.com>
2026-06-29 05:16:09 +0100

    api url changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://backfire-cloning-factsheet.ngrok-free.dev/",
   timeout: 12000,
 });
 
